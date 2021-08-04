aba6174d991ac856b0294c79910b838d26a606cd
danb123 <ohda456@gmail.com>
2021-08-04 07:13:43 +0530

    PlanYourRoute Data Submission

--- src/Components/PlanYourTour.js
+++ src/Components/PlanYourTour.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function PlanYourTour() {
+    const[startDate, setStartDate] = useState(new Date());
     return (
         <div>
-            <h2>You're warmly welcome.. Let me help to assist in planning your safari tour..</h2>
+            <form onSubmit={sendDate}>
+                <h2>You're warmly welcome.. Let me help to assist in planning your safari tour..</h2>
+                <label for="sel1">Select your preference</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Available tours</option>
+                    <option value="Yala">Yala Safari</option>
+                    <option value="Bundala">Bundala Safari</option>
+                    <option value="Wilpatthu">Wilpatthu Safari</option>
+                </select>
+                <label for="sel1">Planned date</label>
+                <DatePicker selected={startDate} onChange={(date)=>setStartDate(date)}/>
+                <button type="submit" className="btn btn-primary">
+                        Submit
+                </button>
+            </form>
         </div>
     )
 }
